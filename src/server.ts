440da971c931c5d5d1a38c1bfa1dd58fde066183
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-10-13 23:23:36 -0300

    initial database structure

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express from 'express';
+
+import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello world' });
 });
 
 app.listen(3333);
