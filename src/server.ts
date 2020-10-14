a114df280a5b1538b7cc9810cf6e39e0357ebb64
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-10-13 23:08:26 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(3333);
