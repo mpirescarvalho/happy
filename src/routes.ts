a937f8b87e9b77c62fcb8916aa16a115e1b6a0f5
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-10-13 23:51:18 -0300

    list and show orphanages

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.get('/orphanages', OrphanagesController.index);
 
 export default routes;
